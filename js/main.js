86adae88353a79e2be0e8f366a2488a2e4e62ca0
Eric <Ericrius1@gmail.com>
2013-08-24 11:08:12 -0700

    mapping photos from instagram

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 var OM = OM || {}
 
+OM.photos = [];
+$(function() {
+  $('.instagram').on('didLoadInstagram', function(event, response) {
+    response.data.map(function(photo) {
+       OM.photos.push(photo.images.standard_resolution.url); 
+    });
+      OM.Galaxy.init();
+  });
 
-OM.Galaxy.init();
+  $('.instagram').instagram({
+    hash: 'omies',
+    clientId: 'cf4ba7af04c942d0a1a141253b04fd16'
+  });
+
+});
